28de2c30841dd9dfd78a32b095fdcecbbcee810f
mohi86 <mohi86@live.com>
2017-04-14 21:50:04 +1200

    Main TodoApp component added

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TodoApp extends Component {
+    render() {
+        return(
+            <div>
+                TodoApp main container
+            </div>
+        )
+    }
+}
+
+export default TodoApp;
