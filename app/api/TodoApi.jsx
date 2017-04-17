90cb836da4131796d4ca28ffb811d2a77c4ddbdf
mohi86 <mohi86@live.com>
2017-04-17 18:56:28 +1200

    Search component wired up

--- app/api/TodoApi.jsx
+++ app/api/TodoApi.jsx
@@ -17,5 +17,30 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos(todos, showCompleted, searchText) {
+        let filteredTodos = todos;
+        //filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+        //filter by search text
+        filteredTodos = filteredTodos.filter((todo) => {
+            let text = todo.text.toLocaleLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        //sort todos by non completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -58,11 +58,13 @@ class TodoApp extends Component {
     }
 
     render() {
-        let {todos} = this.state;
+        let {todos, showCompleted, searchText} = this.state;
+        const filteredTodos = TodoApi.filterTodos(todos, showCompleted, searchText);
+
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={todos} onToggle={this.handleToggle}/>
+                <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
                 <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
         )
